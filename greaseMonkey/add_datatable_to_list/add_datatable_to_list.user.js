a8b271426d68801e26bdbaf995c29fe63208f71f
Charles Bonnissent <charles.bonnissent@anakeen.com>
2011-12-15 14:52:16 +0100

    Add a script that inject datable in dynacase list representation

---
+++ greaseMonkey/add_datatable_to_list/add_datatable_to_list.user.js
@@ -0,0 +1,43 @@
+// ==UserScript==
+// @name           Add Datatable to list
+// @namespace      http://bonnissent.fr
+// @author        Charles bonnissent
+// @homepage      http://www.bonnissent.fr
+// @include        http://*/*dirid=*
+// @resource    JQUERY_JS http://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js
+// @resource    DATATABLE  http://datatables.net/download/build/jquery.dataTables.js
+// @resource    DATATABLE_CSS  http://live.datatables.net/media/css/demo_table.css
+// ==/UserScript==
+
+
+function addJsInline(text) {
+	var GM_JQ = document.createElement('script');
+	GM_JQ.type = 'text/javascript';
+	GM_JQ.textContent = text;
+	document.getElementsByTagName('head')[0].appendChild(GM_JQ);
+}
+
+function addCssInline(text) {
+	var GM_JQ = document.createElement('style');
+	GM_JQ.type = 'text/css';
+	GM_JQ.textContent = text;
+	document.getElementsByTagName('head')[0].appendChild(GM_JQ);
+}
+
+
+
+addJsInline("if (window.jQuery == undefined) { "+GM_getResourceText ("JQUERY_JS")+"}");
+addJsInline(GM_getResourceText("DATATABLE"));
+//addCssInline(GM_getResourceText("DATATABLE_CSS"));
+
+addJsInline("$(document).ready( function() { \
+	var tab = $('#terrible');\
+var nbColomn = $(tab.find('tr')[0]).find('td').length;\
+var thead = '<thead><tr>';\
+for( var i = 0; i < nbColomn; i++) {\
+    thead += '<th></th>';\
+}\
+thead += '</tr></thead>';\
+tab.append(thead);\
+tab.dataTable({'sPaginationType': 'full_numbers'});\
+});");
